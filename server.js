d1b4ca4ea3f84620b918b2f3ce80c5ffe3cd8454
Alex Satur <alexsatur@Alexs-MacBook-Pro.local>
2019-01-08 10:54:23 +0000

    first

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ app.use(bodyParser.json())
 
 app.use(express.static('public'))
 
+var machineOne = {
+  "name" : "Alex",
+  "currentJph" : 0,
+  "currentState" : "Default",
+  "topFaults" : []
+}
+
+
 app.get('/alex', function(req, res){
-  res.send("Hello")
+  res.send(machineOne)
 })
 
 app.listen(process.env.PORT || 8080, () => console.log('all is ok'))
