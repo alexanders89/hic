7f7164ac5c7b2876785a35b52891d0b589a6303d
Alex Satur <alexsatur@Alexs-MacBook-Pro.local>
2019-01-08 10:57:55 +0000

    first

--- server.js
+++ server.js
@@ -7,15 +7,29 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 var machineOne = {
-  "name" : "Alex",
+  "name" : "Machine One",
   "currentJph" : 0,
   "currentState" : "Default",
   "topFaults" : []
 }
 
+function update(newJph, newState, newFault){
+  machineOne.currentJph = newJph
+  machineOne.currentState = newState
+  machineOne.topFaults = newFault
+}
 
-app.get('/alex', function(req, res){
+app.get('/machineone', function(req, res){
   res.send(machineOne)
 })
+
+app.put('/machineOne', function(req, res){
+  newJph = req.body.newJph
+  newState = req.body.newState
+  newFault = req.body.newFault
+  update(newJph, newState, newFault)
+  res.send(machineOne)
+})
+
 
 app.listen(process.env.PORT || 8080, () => console.log('all is ok'))
