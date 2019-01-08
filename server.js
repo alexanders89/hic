58075b076b5e41e93e335ee58dda22b0202138f8
Alex Satur <alexsatur@Alexs-MacBook-Pro.local>
2019-01-08 10:48:45 +0000

    first

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.use(bodyParser.json())
 
 app.use(express.static('public'))
 
+app.get('/alex', function(req, res){
+  res.send("Hello")
+})
+
 app.listen(process.env.PORT || 8080, () => console.log('all is ok'))
