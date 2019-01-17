67a861028c4282eabcc953f42395407f9b26fa95
Alex Satur <alexsatur@Alexs-MacBook-Pro.local>
2019-01-17 13:26:03 +0000

    added twilio

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+const accountSid = 'AC85d63b6b199aa2e579f511eaa1f74c44';
+const authToken = '643f6a8a91ec054bb6bc1e8fdca02ac4';
+const client = require('twilio')(accountSid, authToken);
+
+
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({
@@ -53,6 +58,24 @@ app.post('/machine/:id', function(req, res){
   update(machineNumber, req.body)
   res.send("Hello!")
 })
+
+app.post('/test', function(req, res){
+  sendMessage()
+  res.send("Hello!")
+})
+
+function sendMessage(){
+  client.messages
+  .create({
+    body: `Hi, thanks for texting me!`,
+    from: '+441412807187',
+    to: '07791415382'
+  })
+  .then(message => console.log(message.sid))
+  .done();
+}
+
+
 
 
 app.listen(process.env.PORT || 8080, () => console.log('all is ok'))
