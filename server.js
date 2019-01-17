84a59adfa917fff0612367243b0ab8bcfce01ee9
Alex Satur <alexsatur@Alexs-MacBook-Pro.local>
2019-01-17 14:52:08 +0000

    barry thebiscuit

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/machine/:id', function(req, res){
 
 app.post('/test', function(req, res){
   const twiml = new MessagingResponse();
-  twiml.message('The Robots are coming! Head for the hills!');
+  twiml.message(req.body.body);
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 })
