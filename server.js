d8c064b4f22783804728136f4cea0f265dfaeec3
Alex Satur <alexsatur@Alexs-MacBook-Pro.local>
2019-01-17 14:54:57 +0000

    cup of water

--- server.js
+++ server.js
@@ -70,7 +70,9 @@ app.post('/machine/:id', function(req, res){
 
 app.post('/test', function(req, res){
   const twiml = new MessagingResponse();
-  twiml.message(req.body.body);
+  console.log("Sending message")
+  console.log(req.body)
+  twiml.message('The Robots are coming! Head for the hills!');
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 })
