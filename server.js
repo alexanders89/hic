ada50077e6e4aaef2150c3fef2893c1e0fe772e8
Alex Satur <alexsatur@Alexs-MacBook-Pro.local>
2019-01-17 14:48:03 +0000

    owl

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const TOPHONENUMBER = process.env.toNumber
 
 const client = require('twilio')(ACCOUNTSID, AUTHTOKEN);
 
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
 
 app.use(bodyParser.json())
 
@@ -73,15 +75,10 @@ app.post('/test', function(req, res){
 })
 
 function sendMessage(){
-  console.log("Sending Text")
-  client.messages
-  .create({
-    body: `Hi, thanks for texting me!`,
-    from: `+${FROMPHONENUMBER}`,
-    to: TOPHONENUMBER
-  })
-  .then(message => console.log(message.sid))
-  .done();
+  const twiml = new MessagingResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 }
 
 app.listen(process.env.PORT || 8080, () => console.log('all is ok'))
